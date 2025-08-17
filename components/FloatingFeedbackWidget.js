d8133326fa5eb11d858b4713d5b75dee364ca3db
JihyeAnnaOh <z5442121@zmail.unsw.edu.au>
2025-08-17 15:34:49 +1000

    Update FloatingFeedbackWidget.js

--- components/FloatingFeedbackWidget.js
+++ components/FloatingFeedbackWidget.js
@@ -1,4 +1,5 @@
 'use client';
+/* eslint-disable react/no-unescaped-entities */
 
 import { useState, useEffect } from 'react';
 import { collection, addDoc, serverTimestamp } from 'firebase/firestore';
